718293d810f97cd7cadf802d9c65850be024e794
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-06-29 18:02:10 -0700

    App functional trouble shoot 4 last test

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import manageBand from '../reducers/manageBand'
+import BandInput from './BandInput'
 
 class BandsContainer extends Component {
   render() {
+    const renderBands = this.props.bands.map((band, idx) => <li key={idx}>{band.name}</li>)
     return(
       <div>
-        BandsContainer
+        <BandInput bands={this.props.bands} addBand={this.props.addBand}/>
+        {renderBands}
       </div>
     )
   }
 }
 
-export default BandsContainer
+
+const mapStateToProps = (state) => {
+  return {
+    bands: state.bands
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    addBand: (bandName) => dispatch({type: "ADD_BAND", payload: bandName})
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
